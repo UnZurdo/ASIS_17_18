190376d41d5d8f0aaf4791429e7412c0bafa3e2a
DanFzgz <fraile.daniel@yahoo.es>
2018-03-23 19:26:37 +0100

    Script 6.6

--- practica2/practica2_6.sh
+++ practica2/practica2_6.sh
@@ -1,24 +1,21 @@
+
 #!/bin/bash
-directorios=`find /home/"$USER"/bin*/ -type d 2> /dev/null`
+directorios=`find $HOME/bin{[:alnum:],3} -type d 2> /dev/null`
 path=""
 if [ -z "$directorios" ]
 then
   path=`mktemp -d /home/"$USER"/binXXX`
-  echo "Se ha creado el directorio $path" | grep -o ".*[^/]"
+  echo "Se ha creado el directorio "$path"" | grep -o ".*[^/]"
 else
   path=`stat -c "%n=%Y" $directorios | sort -n -t = -k 2 | grep -o "\/.*\/" | head -n 1`
 fi
 
-echo "Directorio destino de copia: $path" | grep -o ".*[^/]"
-
-copiados=`cp -v ./*.* "$path" 2> /dev/null`
+echo "Directorio destino de copia: "$path"" | grep -o ".*[^/]"
+ejecutables=`find -maxdepth 1 -mindepth 1 -type f -printf '%f\n'`
 ncopias=0
-for i in $copiados; do
-  copiado=`echo "$i" | grep -o "\.\/.*\..*" | tr -d [=\'=]`
-  if [ ! -z "$copiado" ]
-  then
-    echo "$copiado ha sido copiado a $path" | grep -o ".*[^/]"
-    ncopias=$((ncopias+1))
-  fi
-  done
-  echo "Se han copiado $ncopias archivos"
+for i in $ejecutables; do
+  cp ./"$i" "$path"
+  echo "./"$i" ha sido copiado a "$path"" | grep -o ".*[^/]"
+  ncopias=$((ncopias+1))
+done
+echo "Se han copiado "$ncopias" archivos"
