be75b8c55a0a7864e4ed32f86f9be1792c538ec6
DanFzgz <fraile.daniel@yahoo.es>
2018-03-23 16:20:06 +0100

    Script 6.2

--- practica2/practica2_6.sh
+++ practica2/practica2_6.sh
@@ -4,22 +4,17 @@ path=""
 if [ -z "$directorios" ]
 then
   path=`mktemp -d /home/"$USER"/binXXX`
+  echo "Se ha creado el directorio "$path""
 else
   path=`stat -c "%n=%Y" $directorios | sort -n -t = -k 2 | grep -o "\/.*\/" | head -n 1`
 fi
 
-echo "Se ha creado el directorio $path"
-echo "Directorio destino de copia: $path"
-
-copiados=`cp -v ./*.* "$path"`
+echo "Directorio destino de copia: "$path""
+ejecutables=`find -maxdepth 1 -mindepth 1 -type f -printf '%f\n'`
 ncopias=0
-for i in $copiados; do
-  copiado=`echo "$i" | grep -o "\.\/.*\..*" | tr -d [=\'=]`
-  #echo "Copiado vale: $copiado"
-  if [ ! -z "$copiado" ]
-  then
-    echo "$copiado ha sido copiado a $path"
-    ncopias=$((ncopias+1))
-  fi
-  done
-  echo "Se han copiado $ncopias archivos"
+for i in $ejecutables; do
+  cp ./"$i" "$path"
+  echo "./$i ha sido copiado a "$path""
+  ncopias=$((ncopias+1))
+done
+echo "Se han copiado $ncopias archivos"
