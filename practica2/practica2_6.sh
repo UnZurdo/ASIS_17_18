902834499fbbe8296f5e0b1ba4d777d3a37e875e
DanFzgz <fraile.daniel@yahoo.es>
2018-03-23 19:56:42 +0100

    Script 6.8

--- practica2/practica2_6.sh
+++ practica2/practica2_6.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #Jorge Fernandez (721529) y Daniel Fraile (721525)
-directorios=`find $HOME/bin{[:alnum:],3} -type d 2> /dev/null`
+directorios=`find $HOME -type d -name '*bin???' 2> /dev/null`
 path=""
 if [ -z "$directorios" ]
 then
   path=`mktemp -d $HOME/binXXX`
   echo "Se ha creado el directorio "$path""
 else
-  path=`stat -c "%n=%Y" $directorios | sort -n -t = -k 2 | head -n 1`
+  path=`stat -c "%n=%Y" $directorios | sort -n -t = -k 2 | grep -o ".*\="  | tr -d [=\==] | head -n 1`
 fi
 
 echo "Directorio destino de copia: "$path""
 ncopias=0
 for i in ./*; do
-  if [ -x "$i"]
+  if [ -x "$i" ]
   then
     cp "$i" "$path"
     echo ""$i" ha sido copiado a "$path""
