a90801ad6f07cd44fb16a115a91b5732ae23979f
DanFzgz <fraile.daniel@yahoo.es>
2018-03-23 15:36:38 +0100

    Casi 6

--- practica2/practica2_6.sh
+++ practica2/practica2_6.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 directorios=`find /home/"$USER"/bin*/ -type d 2> /dev/null`
+path=""
 if [ -z "$directorios" ]
 then
-  mkdir /home/"$USER"/bin123
-
-  echo "Se ha creado el directorio /home/"$USER"/bin123"
-  echo "Directorio destino de copia: /home/"$USER"/bin123"
-
-  copiados=`cp -v ./*.* /home/"$USER"/bin123`
-  for i in $copiados; do
-    echo "$i" | grep -o "\.\/.*\..*\'"
-  done
-
+  path=`mktemp -d /home/"$USER"/binXXX`
 else
-  #hacer un loop para buscar el directorio y copiar ahi
-  echo "Encontrado"
+  path=`stat -c "%n=%Y" $directorios | sort -n -t = -k 2 | grep -o "\/.*\/" | head -n 1`
 fi
 
-#rm -r /home/"$USER"/bin123/
+echo "Se ha creado el directorio $path"
+echo "Directorio destino de copia: $path"
+
+copiados=`cp -v ./*.* "$path"`
+ncopias=0
+for i in $copiados; do
+  copiado=`echo "$i" | grep -o "\.\/.*\..*"`
+  #echo "Copiado vale: $copiado"
+  if [ ! -z "$copiado" ]
+  then
+    echo "$copiado ha sido copiado a $path"
+    ncopias=$((ncopias+1))
+  fi
+  done
+  echo "Se han copiado $ncopias archivos"
+#rm -r "$path"
