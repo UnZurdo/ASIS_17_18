73fe43dbd74191c28951fd7b3a31bc666be73ad7
jorge97fernandez <jorge97fernandez@gmail.com>
2018-03-23 16:56:45 +0100

    Version del script 6

--- practica2/practica2_6.sh
+++ practica2/practica2_6.sh
@@ -1,21 +1,24 @@
-
 #!/bin/bash
 directorios=`find /home/"$USER"/bin*/ -type d 2> /dev/null`
 path=""
 if [ -z "$directorios" ]
 then
   path=`mktemp -d /home/"$USER"/binXXX`
-  echo "Se ha creado el directorio "$path"" | grep -o ".*[^/]"
 else
   path=`stat -c "%n=%Y" $directorios | sort -n -t = -k 2 | grep -o "\/.*\/" | head -n 1`
 fi
 
-echo "Directorio destino de copia: "$path"" | grep -o ".*[^/]"
-ejecutables=`find -maxdepth 1 -mindepth 1 -type f -printf '%f\n'`
+echo "Se ha creado el directorio $path"
+echo "Directorio destino de copia: $path"
+
+copiados=`cp -v ./*.* "$path"`
 ncopias=0
-for i in $ejecutables; do
-  cp ./"$i" "$path"
-  echo "./"$i" ha sido copiado a "$path"" | grep -o ".*[^/]"
-  ncopias=$((ncopias+1))
-done
-echo "Se han copiado "$ncopias" archivos"
+for i in $copiados; do
+  copiado=`echo "$i" | grep -o "\.\/.*\..*" | tr -d [=\'=]`
+  if [ ! -z "$copiado" ]
+  then
+    echo "$copiado ha sido copiado a $path"
+    ncopias=$((ncopias+1))
+  fi
+  done
+  echo "Se han copiado $ncopias archivos"
