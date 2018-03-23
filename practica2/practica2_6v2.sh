4e3524e5a10a033ad5f991334da439e4fa006f4f
Alberto García <UnZurdo@users.noreply.github.com>
2018-03-23 21:20:48 +0100

    Create practica2_6v2.sh

---
+++ practica2/practica2_6v2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#Jorge Fernandez (721529) y Daniel Fraile (721525)
+directorios=$(find $HOME/bin* -maxdepth 0 -type d 2> /dev/null)
+path=""
+if [ -z "$directorios" ]
+then
+  path=`mktemp -d $HOME/binXXX`
+  echo "Se ha creado el directorio $path"
+else
+  path=`stat -c "%n=%Y" $directorios | sort -n -t = -k 2 | grep -o ".*\="  | tr -d [=\==] | head -n 1`
+fi
+
+echo "Directorio destino de copia: $path"
+ncopias=0
+ficheros=( $(find . -not -path '*/\.*' -type f) )
+for i in "${ficheros[@]}"; do
+  if [ -x "$i" ]
+  then
+    cp "$i" "$path"
+    echo ""$i" ha sido copiado a $path"
+    ncopias=$((ncopias+1))
+  fi
+done
+echo "Se han copiado "$ncopias" archivos"
